4d5fa3489f1ec1b88353e7fa11bf4e2c85256820
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-10-19 20:58:25 +0000

    basic version of the game

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
+use std::io;
+
+use rand::Rng;
+
 fn main() {
-    print!("Hello world");
-    println!("Goodbye world");
+    let mut guessed = String::new();
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    io::stdin()
+        .read_line(&mut guessed)
+        .expect("Something went wrong");
+
+    match guessed.trim().parse::<i32>() {
+        Ok(num) => {
+            println!("You entered a vaild number {num}");
+            if num == secret_number {
+                println!("You guessed correctly");
+                return;
+            }
+            println!("You didn't guess the number, it was {secret_number}")
+        }
+        Err(_) => {
+            println!("You didn't enter a valid number")
+        }
+    }
 }
